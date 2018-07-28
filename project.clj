02c499abea3e85bda2723915ad2aa591b6c0bbdf
Pieter Koornhof <koornhof.pieter@gmail.com>
2018-07-28 13:00:42 +0200

    Fix: Project Url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject za.co.simply/compojure.claims "0.1.1-SNAPSHOT"
   :description "A is a small library that allows you to do claims/roles authorisation per Compojure route."
-  :url "https://github.com/SimplyFinServices/compojure.core"
+  :url "https://github.com/SimplyFinServices/compojure.claims"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
