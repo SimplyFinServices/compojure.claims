1c82e62004b081e97e3779524baf83377ac42450
Pieter Koornhof <koornhof.pieter@gmail.com>
2018-07-29 09:21:00 +0200

    Fix: Project Description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject za.co.simply/compojure.claims "1.0.0"
-  :description "A is a small library that allows you to do claims/roles authorisation per Compojure route."
+  :description "A small library that allows you to do claims/roles authorisation per Compojure route."
   :url "https://github.com/SimplyFinServices/compojure.claims"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
